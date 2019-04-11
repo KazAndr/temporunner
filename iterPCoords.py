a7e567d066f6089b875eed13f67b4469bea90479
Andrey Kazantsev <kaz.prao@bk.ru>
2019-04-11 12:15:55 +0300

    Code was optimized

--- iterPCoords.py
+++ iterPCoords.py
@@ -9,6 +9,7 @@ Created on Wed Dec 19 13:31:22 2018
 import os
 
 from copy import copy
+from itertools import product
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +24,6 @@ os.system('rm res_iter_p_coords_' + name_pulsar + '.txt')
 os.system('rm out_res_iter_p_coords_' + name_pulsar + '.log')
 
 add_period_list = list(range(100))
-# add_period_list = [i for i in add_period_list if str(i)[-1] != '0']
 
 with open(name_pulsar + '_start.par', 'r') as file:
     lines = file.readlines()
@@ -38,41 +38,37 @@ ra_start = copy(coords.ra) - 5*u.arcmin
 dec_start = copy(coords.dec) - 12.5*u.arcmin
 start_period = copy(lines[3][:-1])
 
-for add in tqdm(add_period_list):
-    ra = copy(ra_start)
+ra_list = [ra_start + i*u.arcsec for i in range(5, 600, 5)]
+dec_list = [dec_start + i*u.arcsec for i in range(10, 1500, 10)]
 
-    for i in tqdm(range(120), leave='False'):
-        ra += 5*u.arcsec
-        dec = copy(dec_start)
+elem_list = list(product(add_period_list, ra_list, dec_list))
 
-        for j in tqdm(range(150), leave='False'):
-            dec += 10*u.arcsec
+for elements in tqdm(elem_list):
+    lines[1] = 'RAJ       ' + elements[1].to_string(sep=':') + '\n'
 
-            lines[1] = 'RAJ       ' + ra.to_string(sep=':') + '\n'
+    lines[2] = 'DECJ       ' + elements[2].to_string(sep=':') + '\n'
 
-            lines[2] = 'DECJ       ' + dec.to_string(sep=':') + '\n'
+    lines[3] = start_period + str(elements[0]) + '    1' + '\n'
 
-            lines[3] = start_period + str(add) + '0' + '    1' + '\n'
+    lines[4] = 'F1       ' + '0.0' + '     1' + '\n'
 
-            lines[4] = 'F1       ' + '0.0' + '     1' + '\n'
+    with open(name_pulsar + '.par', 'w') as file:
+        for line in lines:
+            file.write(line)
 
-            with open(name_pulsar + '.par', 'w') as file:
-                for line in lines:
-                    file.write(line)
+    os.system('tempo ' + name_pulsar + '.tim > outtempo.log')
+    os.system(
+            '~/work/tempo/util/print_resid/./print_resid -mre > ' +
+            'resid_' + name_pulsar + '.ascii')
 
-            os.system('tempo ' + name_pulsar + '.tim > outtempo.log')
-            os.system(
-                    '~/work/tempo/util/print_resid/./print_resid -mre > ' +
-                    'resid_' + name_pulsar + '.ascii')
+    data = np.genfromtxt('resid_' + name_pulsar + '.ascii').T
 
-            data = np.genfromtxt('resid_' + name_pulsar + '.ascii').T
-
-            with open('res_iter_p_coords_' + name_pulsar + '.txt', 'a') as file:
-                file.write(start_period[11:] + str(add) + ' ')
-                file.write(ra.to_string(sep=':') + ' ')
-                file.write(dec.to_string(sep=':') + ' ')
-                file.write(str(np.std(data[1])))
-                file.write('\n')
+    with open('res_iter_p_coords_' + name_pulsar + '.txt', 'a') as file:
+        file.write(start_period[11:] + str(elements[0]) + ' ')
+        file.write(elements[1].to_string(sep=':') + ' ')
+        file.write(elements[2].to_string(sep=':') + ' ')
+        file.write(str(np.std(data[1])))
+        file.write('\n')
 
 
 data = np.genfromtxt('res_iter_p_coords_' + name_pulsar + '.txt').T
