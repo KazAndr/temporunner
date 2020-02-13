af80c56c970861d76798324901f250bfcdec7552
Kazantsev Andrey <kaz.prao@bk.ru>
2020-02-13 16:00:56 +0300

    Update config.py
    Конфигурационному файлу добавлены уточняющие комментарии и добавлена переменная связанная с добавлением функционала перебора по миллисекундам.

--- test/config.py
+++ test/config.py
@@ -1,12 +1,23 @@
+# Setup name of pulsar and working directory
 name_pulsar = '1112'
-work_dir = '/home/andr/Yandex.Disk/1.Работа/TIMING/tulbashev/original_data/1112+50/'
+work_dir = (
+    f'/home/andr/Yandex.Disk/1.Работа/'
+    f'TIMING/tulbashev/original_data/1112+50/'
+)
+
+# Setup range for brute force period/frequency
 period_range = 10
 period_step = 1
+
+# Setup range for brute force coordinates
 ra_range = 5  # arcmin
 dec_range = 12.5  # arcmin
+
+# Setup dimension and step for brute force coordinates
+dimension = 'arcsec'
 ra_step_bf = 5  # arcsec
 dec_step_bf = 10  # arcsec
-ra_bruteforce = int(ra_range*2*60 + ra_step_bf)  # arcsec
-dec_bruteforce = int(dec_range*2*60 + dec_step_bf)  # arcsec
+
+# on/off fitting params
 fit_period = '1'
 fit_coords = ''
