cf8af5f5c344cab555f2df9be7066f81c86c7649
Kazantsev Andrey <kaz.prao@bk.ru>
2020-06-13 17:05:12 +0300

    Create config.py
    Добавлен конфигурационный файл.

---
+++ config.py
@@ -0,0 +1,21 @@
+# Setup name of pulsar and working directory
+name_pulsar = '1112'
+work_dir = (
+    f'/home/andr/Yandex.Disk/1.Работа/'
+    f'TIMING/tulbashev/original_data/1112+50/'
+)
+
+# Setup range for brute force period/frequency
+period_range = 10
+
+# Setup range for brute force coordinates
+ra_range = '0h02m00.0s'
+dec_range = '0d30m00.0s'
+
+# Setup dimension and step for brute force coordinates
+step_ra = '0h00m05.0s'
+step_dec = '0d00m24.0s'
+
+# on/off fitting params
+fit_period = '1'
+fit_coords = '1'
