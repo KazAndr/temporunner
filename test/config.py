e9ba2c4c35bbe646f1303ae5d3aea9a8fb0581cb
Kazantsev Andrey <kaz.prao@bk.ru>
2020-06-13 17:09:00 +0300

    Update config.py
    Тестовый конфигурационный файл изменен согласно текущей реализации программы.

--- test/config.py
+++ test/config.py
@@ -1,22 +1,17 @@
 # Setup name of pulsar and working directory
 name_pulsar = '1112'
-work_dir = (
-    f'/home/andr/Yandex.Disk/1.Работа/'
-    f'TIMING/tulbashev/original_data/1112+50/'
-)
+work_dir = ('./')
 
 # Setup range for brute force period/frequency
 period_range = 10
-period_step = 1
 
 # Setup range for brute force coordinates
-ra_range = 1  # arcmin
-dec_range = 1  # arcmin
+ra_range = '0h02m00.0s'
+dec_range = '0d05m00.0s'
 
 # Setup dimension and step for brute force coordinates
-dimension = 'marcsec'  # arcsec or marcsec
-ra_step_bf = 500  # arcsec
-dec_step_bf = 500  # arcsec
+step_ra = '0h00m05.0s'
+step_dec = '0d00m24.0s'
 
 # on/off fitting params
 fit_period = '1'
