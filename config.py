4cd4d04dc87a5e3199c0ee115eb5071eeacec78b
DmitrijAndreev <dima.andreev.2005@inbox.ru>
2022-01-20 23:16:17 +0300

    brute-force_with_PINT

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 # Setup name of pulsar and working directory
 name_pulsar = '1112'
-work_dir = ('./')
+work_dir = (
+    f'/home/andr/Yandex.Disk/1.Работа/'
+    f'TIMING/tulbashev/original_data/1112+50/'
+)
 
 # Setup range for brute force period/frequency
 period_range = 10
 
 # Setup range for brute force coordinates
 ra_range = '0h02m00.0s'
-dec_range = '0d05m00.0s'
+dec_range = '0d30m00.0s'
+freq_range = 100
 
 # Setup dimension and step for brute force coordinates
 step_ra = '0h00m05.0s'
@@ -16,3 +20,21 @@ step_dec = '0d00m24.0s'
 # on/off fitting params
 fit_period = '1'
 fit_coords = '1'
+fit_freq = '1'
+
+
+ra_brut = '+'
+dec_brut = '+'
+freq_brute = '+'
+
+
+par_stop = '-'
+
+period_half = 828219.8799685
+
+residuals = 0.0
+
+
+
+
+
--- iterPCoords.py
+++ iterPCoords.py
@@ -1,27 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 19 13:31:22 2018
-
-@author: Kazantsev Andrey
-"""
-
 import os
 import sys
+import subprocess
 
 from copy import copy
 from itertools import product
+from subprocess import Popen
 
 import numpy as np
-import matplotlib.pyplot as plt
-
 from astropy.coordinates import SkyCoord
 from astropy import units as u
+from astropy.coordinates import Angle
 from tqdm import tqdm
+
 
 sys.path.append('.')  # Добавляем локальный путь запуска файла для config
 
 from config import *
+
 
 # Удаление результатов от предыдущей обработки
 os.system(f'rm res_iter_p_coords_{name_pulsar}.txt')
@@ -42,42 +37,56 @@ coords = SkyCoord(
         + dec_coord[0],
         unit=(u.deg, u.deg))
 
-if dimension == 'arcsec':
-    dim = u.arcsec
-    ra_bruteforce = int(ra_range*2*60 + ra_step_bf)  # arcsec
-    dec_bruteforce = int(dec_range*2*60 + dec_step_bf)  # arcsec
-elif dimension == 'marcsec':
-    dim = u.milliarcsecond
-    ra_bruteforce = int(ra_range*2*60*100 + ra_step_bf)  # marcsec
-    dec_bruteforce = int(dec_range*2*60*100 + dec_step_bf)  # marcsec
+
+
+# Подгрузка диапазона итерации
+ra_range = Angle(ra_range)
+dec_range = Angle(dec_range)
+
+ra_start = Angle(ra_coord[0], 'hour')
+dec_start = copy(coords.dec)
+
+ra_stop_2 = ra_start - ra_range
+dec_stop_2 = dec_start - dec_range
+
+ra_stop = ra_start + ra_range
+dec_stop = dec_start + dec_range
+
+step_freq = 10**-len(start_period[0])
+
+# Подгрузка  шага итерации
+step_ra = Angle(step_ra)
+step_dec = Angle(step_dec)
+
+# Вычисление количества необходимых итераций
+numbers_ra = int(round((ra_range/step_ra).value))
+numbers_dec = int(round((dec_range/step_dec).value))
+# Создание словарей, взависимости от типа перебора (вверх или вниз)
+if ra_brut == '+':
+    ra_brutforce = np.linspace(ra_start, ra_stop, numbers_ra)
 else:
-    print('Unknown dimension!')
+    ra_brutforce = np.linspace(ra_start, ra_stop_2, numbers_ra)
 
-ra_start = copy(coords.ra) - ra_range*u.arcmin
-dec_start = copy(coords.dec) - dec_range*u.arcmin
+if dec_brut == '+':
+    dec_brutforce = np.linspace(dec_start, dec_stop, numbers_dec)
+else:
+    dec_brutforce = np.linspace(dec_start, dec_stop_2, numbers_dec)
 
-ra_list = [ra_start + i*dim for i in range(
-    ra_step_bf,
-    ra_bruteforce,
-    ra_step_bf
-)]
+if freq_brute == '+':
+    freq_brutforce = [f'{start_period[0]}{i:02d}' for i in range(freq_range)]
+else:
+    freq_brutforce = [f'{str(float(start_period[0]) - 10**(-(len(start_period[0])-1)))[:len(str(start_period[0]))+1]}{i:02d}' for i in range(1,freq_range)]
+    freq_brutforce.append(str(start_period[0]) + '00')
+    freq_brutforce.reverse()# Создаёт список из частот в переборе от истины в худшую сторону (вниз)
 
-dec_list = [dec_start + i*dim for i in range(
-    dec_step_bf,
-    dec_bruteforce,
-    dec_step_bf
-)]
-
-add_period_list = range(period_step, period_range, period_step)
-
-elem_list = list(product(add_period_list, ra_list, dec_list))
+elem_list = list(product(freq_brutforce, ra_brutforce, dec_brutforce))
 
 for elements in tqdm(elem_list):
     lines[1] = f'RAJ        {elements[1].to_string(sep=":")}    {fit_coords}\n'
 
     lines[2] = f'DECJ       {elements[2].to_string(sep=":")}   {fit_coords}\n'
 
-    lines[3] = f'F0         {start_period[0]}{elements[0]}    {fit_period}\n'
+    lines[3] = f'F0         {elements[0]}    {fit_freq}\n'
 
     lines[4] = f'F1         0.0     1\n'  # Производная подгонятеся всегда
 
@@ -85,21 +94,23 @@ for elements in tqdm(elem_list):
         for line in lines:
             file.write(line)
 
-    os.system(
-        f'tempo -f {name_pulsar}.par {work_dir}{name_pulsar}.tim > outtempo.log')
-    os.system(f'print_resid -mre > resid_{name_pulsar}.ascii')
+    cmd_tempo = (f'tempo -f {name_pulsar}.par {name_pulsar}.tim')
+    run_tempo = Popen(cmd_tempo.split(), stdout=subprocess.PIPE)
+    run_tempo.wait()
+
+    with open(f'resid_{name_pulsar}.ascii', 'w') as file:
+        cmd_save_resid = f'print_resid -mre'
+        run_save = Popen(cmd_save_resid.split(), stdout=file)
+        run_save.wait()
 
     data = np.genfromtxt(f'resid_{name_pulsar}.ascii').T
 
     with open(f'res_iter_p_coords_{name_pulsar}.txt', 'a') as file:
-        file.write(f'{start_period[0]}{elements[0]}' + ' ')
+        file.write(elements[0] + ' ')
         file.write(elements[1].to_string(sep=':') + ' ')
         file.write(elements[2].to_string(sep=':') + ' ')
         file.write(str(np.std(data[1])))
-        file.write('\n')
-
-
-data = np.genfromtxt(f'res_iter_p_coords_{name_pulsar}.txt').T
-plt.close()
-plt.plot(data[3])
-plt.savefig(f'res_iter_p_coords_{name_pulsar}.png', format='png', dpi=150)
+        file.write('\n') # Создание файла-таблицы с комбинацией параметров и размахом остаточных уклонений
+    residuals = np.std(data[1])
+    if np.std(data[1]) > period_half and par_stop == '+':
+        break
